cf2fe90dafcfc08761244af3b9ff4f0a73fb77e0
Shivayan <147698014+Shivayan09@users.noreply.github.com>
2025-03-31 19:04:07 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./*.html"], // Ensure Tailwind scans your HTML files
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
